8d9609dae1596150b14697c92b8a3e885be48d92
sutungpo <sutungpo@qq.com>
2024-06-05 09:53:58 +0800

    mobilesam

--- serverless/pytorch/facebookresearch/sam/nuclio/model_handler.py
+++ serverless/pytorch/facebookresearch/sam/nuclio/model_handler.py
@@ -4,19 +4,29 @@
 
 import numpy as np
 import torch
-from segment_anything import sam_model_registry, SamPredictor
+from mobilesamv2 import sam_model_registry, SamPredictor
+
 
 class ModelHandler:
+
     def __init__(self):
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.sam_checkpoint = "/opt/nuclio/sam/sam_vit_h_4b8939.pth"
-        self.model_type = "vit_h"
+        self.device = torch.device(
+            'cuda' if torch.cuda.is_available() else 'cpu')
+        mobilesamv2 = sam_model_registry['vit_h']()
+        mobilesamv2.to(device=self.device)
+        mobilesamv2.eval()
+        encoder_path = {
+            'efficientvit_l2': '/opt/nuclio/sam/MobileSAMv2/weight/l2.pt'
+        }
+        encoder_type = "efficientvit_l2"
+        image_encoder = sam_model_registry[encoder_type](
+            encoder_path[encoder_type]).to(device=self.device)
+        image_encoder.eval()
+        mobilesamv2.image_encoder = image_encoder
         self.latest_image = None
-        sam_model = sam_model_registry[self.model_type](checkpoint=self.sam_checkpoint)
-        sam_model.to(device=self.device)
-        self.predictor = SamPredictor(sam_model)
+        self.predictor = SamPredictor(mobilesamv2)
 
     def handle(self, image):
         self.predictor.set_image(np.array(image))
-        features = self.predictor.get_image_embedding()
+        features = self.predictor.features
         return features
