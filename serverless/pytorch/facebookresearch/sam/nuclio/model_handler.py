c8e4fd0b8d8c5d0704802743bc5ab5e8f52bd814
sutungpo <sutungpo@qq.com>
2024-06-06 23:12:44 +0800

    sam changed

--- serverless/pytorch/facebookresearch/sam/nuclio/model_handler.py
+++ serverless/pytorch/facebookresearch/sam/nuclio/model_handler.py
@@ -9,8 +9,8 @@ from segment_anything import sam_model_registry, SamPredictor
 class ModelHandler:
     def __init__(self):
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.sam_checkpoint = "/opt/nuclio/sam/sam_vit_h_4b8939.pth"
-        self.model_type = "vit_h"
+        self.sam_checkpoint = "/opt/nuclio/sam/sam_vit_b_01ec64.pth"
+        self.model_type = "vit_b"
         self.latest_image = None
         sam_model = sam_model_registry[self.model_type](checkpoint=self.sam_checkpoint)
         sam_model.to(device=self.device)
